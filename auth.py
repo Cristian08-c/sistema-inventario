9f8c553a9cf60d6d3ca01b6893211c4a0e204f84
Safestt <126298996+Safestt@users.noreply.github.com>
2025-10-28 02:52:09 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@ from models import Login, Register
 from utils.auth_util import hash_password,verifypassword
 from db import *
 
-router = APIRouter(prefix="/auth",tags=["Auth"])
+Router = APIRouter(prefix="/auth",tags=["Auth"])
 
 
 
