bf63a3261af866c204029e841eeb227cf7405bc5
Safestt <zakbass280@gmail.com>
2025-10-27 23:38:09 -0500

    Refactor authentication logic: replace hardcoded user verification with database lookup

--- auth.py
+++ auth.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI,APIRouter, HTTPException
 from models import Login, Register
 from utils.auth_util import hash_password,verifypassword
+from db import *
 
 Router = APIRouter(prefix="/auth",tags=["Auth"])
 
-userdb =  "$2b$12$.sxw3jhvE3RWiwsspEnSWuYYfoyljE8JSQu1VKJyKwxRgRUhrDwHa"
+
 
 @Router.post("/login/")
 def Login(credentials: Login):
     user = credentials.Username
     password = credentials.Password
 
-    if not verifypassword(password,userdb):
-        raise HTTPException(status_code=400,detail="Credenciales incorrectas")
-    else:
-        raise HTTPException(status_code=200,detail="Login exitoso")
+
+    User,HashedPassword = read_user_by_username(user)
+
+    if User:
+        if not verifypassword(password,HashedPassword):
+            raise HTTPException(status_code=400,detail="Credenciales incorrectas")
+        else:
+            raise HTTPException(status_code=200,detail="Login exitoso")
 
 
 @Router.post("/register/")
@@ -24,6 +29,8 @@ def Login(credentials: Register):
     password = credentials.Password
 
     HashedData = hash_password(password)
+
+    create_user(user,HashedData,"none")
 
     raise HTTPException(status_code=200,detail="Registro exitoso")
 
---
+++ db.py
@@ -0,0 +1,39 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+
+supabase: Client = create_client(url, key)
+
+def create_user(username, password, role):
+    data = {
+        "username": username,
+        "password": password,
+        "role": role
+    }
+    response = supabase.table("users").insert(data).execute()
+    print("Usuario creado:", response.data)
+
+
+
+
+
+def read_user_by_username(username):
+    data = {"username"}
+
+    response = supabase.table("users").select("*").eq("username",username).execute()
+    if not response.data:
+        print("User no encontado")
+    else:
+        return response.data[0]["username"],response.data[0]["password"]
+
+
+user,password = read_user_by_username("Safe")
+
+print(password)
